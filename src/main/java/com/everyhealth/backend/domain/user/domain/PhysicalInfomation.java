66dd84b8f9c7461e7c5b8898dab5029885b09b52
suhhyun524 <2835223@ewhain.net>
2024-02-06 01:50:05 +0900

    [feat] 도메인 및 DTO 작성

--- src/main/java/com/everyhealth/backend/domain/user/domain/PhysicalInfomation.java
+++ src/main/java/com/everyhealth/backend/domain/user/domain/PhysicalInfomation.java
@@ -1,9 +1,11 @@
 package com.everyhealth.backend.domain.user.domain;
 
+import com.everyhealth.backend.domain.user.dto.request.PhysicalInfomationRequestDTO;
 import com.everyhealth.backend.global.entity.PhysicalAbilityLevel;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -24,21 +26,78 @@ public class PhysicalInfomation {
     @Enumerated(EnumType.STRING)
     private PhysicalAbilityLevel physicalAbilityLevel;
 
-    private boolean core;
+    boolean core;
 
-    private boolean rightUpperArm;
+    boolean rightUpperArm;
 
-    private boolean rightLowerArm;
+    boolean rightLowerArm;
 
-    private boolean leftUpperArm;
+    boolean leftUpperArm;
 
-    private boolean leftLowerArm;
+    boolean leftLowerArm;
 
-    private boolean rightUpperLeg;
+    boolean rightUpperLeg;
 
-    private boolean rightLowerLeg;
+    boolean rightLowerLeg;
 
-    private boolean leftUpperLeg;
+    boolean leftUpperLeg;
 
-    private boolean leftLowerLeg;
+    boolean leftLowerLeg;
+
+
+    // 생성자
+    @Builder
+    private PhysicalInfomation(
+            User user,
+            PhysicalAbilityLevel physicalAbilityLevel,
+            boolean core,
+            boolean rightUpperArm,
+            boolean rightLowerArm,
+            boolean leftUpperArm,
+            boolean leftLowerArm,
+            boolean rightUpperLeg,
+            boolean rightLowerLeg,
+            boolean leftUpperLeg,
+            boolean leftLowerLeg) {
+        this.user = user;
+        this.physicalAbilityLevel = physicalAbilityLevel;
+        this.core = core;
+        this.rightUpperArm = rightUpperArm;
+        this.rightLowerArm = rightLowerArm;
+        this.leftUpperArm = leftUpperArm;
+        this.leftLowerArm = leftLowerArm;
+        this.rightUpperLeg = rightUpperLeg;
+        this.rightLowerLeg = rightLowerLeg;
+        this.leftUpperLeg = leftUpperLeg;
+        this.leftLowerLeg = leftLowerLeg;
+    }
+
+    public static PhysicalInfomation of(User user, PhysicalInfomationRequestDTO physicalInfo) {
+        return PhysicalInfomation.builder()
+                .user(user)
+                .physicalAbilityLevel(physicalInfo.getPhysicalAbilityLevel())
+                .core(physicalInfo.isCore())
+                .rightUpperArm(physicalInfo.isRightUpperArm())
+                .rightLowerArm(physicalInfo.isRightLowerArm())
+                .leftUpperArm(physicalInfo.isLeftUpperArm())
+                .leftLowerArm(physicalInfo.isLeftLowerArm())
+                .rightUpperLeg(physicalInfo.isRightUpperLeg())
+                .rightLowerLeg(physicalInfo.isRightLowerLeg())
+                .leftUpperLeg(physicalInfo.isLeftUpperLeg())
+                .leftLowerLeg(physicalInfo.isLeftLowerLeg())
+                .build();
+    }
+
+    public void updatePhysicalInfo(PhysicalInfomationRequestDTO physicalInfo) {
+        this.physicalAbilityLevel = physicalInfo.getPhysicalAbilityLevel();
+        this.core = physicalInfo.isCore();
+        this.rightUpperArm = physicalInfo.isRightUpperArm();
+        this.rightLowerArm = physicalInfo.isRightLowerArm();
+        this.leftUpperArm = physicalInfo.isLeftUpperArm();
+        this.leftLowerArm = physicalInfo.isLeftLowerArm();
+        this.rightUpperLeg = physicalInfo.isRightUpperLeg();
+        this.rightLowerLeg = physicalInfo.isRightLowerLeg();
+        this.leftUpperLeg = physicalInfo.isLeftUpperLeg();
+        this.leftLowerLeg = physicalInfo.isLeftLowerLeg();
+    }
 }
---
+++ src/main/java/com/everyhealth/backend/domain/user/dto/response/LoginResponseDTO.java
@@ -0,0 +1,26 @@
+package com.everyhealth.backend.domain.user.dto.response;
+
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class LoginResponseDTO {
+
+    private String accessToken;
+
+    private boolean isMember;
+
+    @Builder
+    private LoginResponseDTO(String accessToken, boolean isMember) {
+        this.accessToken = accessToken;
+        this.isMember = isMember;
+    }
+
+    public static LoginResponseDTO of(String accessToken, boolean isMember) {
+        return LoginResponseDTO.builder()
+                .accessToken(accessToken)
+                .isMember(isMember)
+                .build();
+    }
+}

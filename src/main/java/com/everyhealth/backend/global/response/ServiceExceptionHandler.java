08f2b3dbf293e0a2f690a99e8e7c6e2a1a09c358
Jeonghyun Lee <hyeon00@ewhain.net>
2024-02-15 13:13:43 +0900

    chore: 클래스명 변경

--- src/main/java/com/everyhealth/backend/global/response/ServiceExceptionHandler.java
+++ src/main/java/com/everyhealth/backend/global/response/ServiceExceptionHandler.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 @RestControllerAdvice
-public class ErrorResponseHandler {
+public class ServiceExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public final ApiResponse<Object> handleAllExceptions(Exception ex, WebRequest request) {

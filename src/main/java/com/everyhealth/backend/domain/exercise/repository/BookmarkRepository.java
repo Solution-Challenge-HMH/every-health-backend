18f25c7943111a0660b79e4d999fe1a51ff92c70
suhhyun524 <2835223@ewhain.net>
2024-02-19 01:57:18 +0900

    [fix] 북마크 삭제 수정

--- src/main/java/com/everyhealth/backend/domain/exercise/repository/BookmarkRepository.java
+++ src/main/java/com/everyhealth/backend/domain/exercise/repository/BookmarkRepository.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
-    Optional<Bookmark> findByUserAndId(User user, Long exerciseId);
+    Optional<Bookmark> findByUserAndExercise(User user, Exercise exercise);
 
     boolean existsByUserAndExercise(User user, Exercise exercise);
 }
